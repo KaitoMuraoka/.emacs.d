6e5b4def93d5480d9bfb81346d04d18ea2144ddc
KaitoMuraoka <kaitoboshi@icloud.com>
2025-01-29 10:23:53 +0900

    add comment out

--- transient/history.el
+++ transient/history.el
@@ -1,7 +1,11 @@
 ((magit-branch nil)
  (magit-commit nil
 	       ("--reset-author"))
+ (magit-diff
+  ("--no-ext-diff"))
  (magit-dispatch nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate"))
  (magit-pull nil)
  (magit-push nil)
  (magit-rebase
