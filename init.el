c95efb0fdb2fceea08442db85b352d9876719ff3
KaitoMuraoka <kaitoboshi@icloud.com>
2025-03-19 11:49:45 +0900

    mode-line にブランチ名を2つ表示しないように修正

--- init.el
+++ init.el
@@ -167,23 +167,6 @@
   :bind ("C-q" . dired-sidebar-toggle-sidebar)
   :custom ((dired-sidebar-theme . 'nerd)))
 
-;; Show Git branch information to mode-line
-(let ((cell (or (memq 'mode-line-position mode-line-format)
-		(memq 'mode-line-buffer-identification mode-line-format)))
-      (newcdr '(:eval (my/update-git-branch-mode-line))))
-  (unless (member newcdr mode-line-format)
-    (setcdr cell (cons newcdr (cdr cell)))))
-
-(defun my/update-git-branch-mode-line ()
-  (let* ((branch (replace-regexp-in-string
-                  "[\r\n]+\\'" ""
-                  (shell-command-to-string "git symbolic-ref -q HEAD")))
-         (mode-line-str (if (string-match "^refs/heads/" branch)
-                            (format "[%s]" (substring branch 11))
-                          "[Not Repo]")))
-    (propertize mode-line-str
-                'face '((:foreground "white" :weight bold)))))
-
 ;; 名前をつけずに新規作成(https://qiita.com/tadsan/items/4ad2e5e3114fff172b6a)
 (defun my/new-untitled-buffer ()
   "Create and switch to untitled buffer."
