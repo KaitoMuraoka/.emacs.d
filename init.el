d55dd70448203fa2f2789e50bb83580a52c98b0e
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-03-24 14:16:42 +0900

    re-setup org-mode (#107)

--- init.el
+++ init.el
@@ -243,109 +243,36 @@
 ;;; ============================================================
 ;;; org-mode
 ;;; ============================================================
+(use-package org
+  :hook (org-mode . visual-line-mode)
+  :custom
+  (org-directory "~/org/")
+  (org-agenda-files '("~/org/todo.org" "~/org/diary.org"))
+  (org-todo-keywords
+   '((sequence "TODO(t)" "DOING(i)" "|" "DONE(d)" "CANCEL(c)")))
+  (org-startup-indented t)
+  (org-hide-leading-stars t)
+  (org-startup-folded 'content)
+  (org-enforce-todo-checkbox-dependencies nil)
 
-;; C-c a でorg-agendaを開く
-(global-set-key (kbd "C-c a") 'org-agenda)
+  ;; org-capture テンプレートもここに移動
+  (org-capture-templates
+   '(("d" "今日の日記" entry
+      (file+olp+datetree "~/org/diary.org")
+      "* %<%H:%M> %?\n"
+      :empty-lines 1)
+     ("t" "TODO追加" entry
+      (file+headline "~/org/todo.org" "TODO")
+      "** TODO %?\n  DEADLINE: %^{期限}t\n  %i\n"
+      :empty-lines 1)
+     ("m" "ミーティングメモ" entry
+      (file+olp+datetree "~/org/diary.org")
+      "* MTG: %^{タイトル}\n** 参加者: %?\n** 内容:\n** アクション:\n"
+      :empty-lines 1)))
 
-;; CLOSED タイムスタンプを自動記録する
-;; TODOをDONEにした時、完了時刻を自動記録する
-(setq org-log-done 'time)
-
-;; TODOキーワードをカスタマイズする
-;; TODO : 未完了(自分ボール)
-;; DOING: 実行中(自分ボール)
-;; WAIT : 停止中
-;; DONE : 完了
-(setq org-todo-keywords
-      '((sequence "TODO" "DOING" "WAIT" "DONE")))
-(setq org-todo-keyword-faces
-      '(
-        ("DOING" . (:foreground "blue"))
-        ("WAIT" . (:foreground "gray"))
-        ))
-
-;; 状態変化に連動してタイマーを制御する
-(defun org-clock-on-state-change()
-  (cond
-   ;; DOINGになったらタイマー開始
-   ((string= org-state "DOING")
-    (org-clock-in))
-   ;; WAITになったらタイマー停止
-   ((string= org-state "WAIT")
-    (when (org-clock-is-active)
-      (org-clock-out)))
-   ;; DONEになったらタイマー停止
-   ((string= org-state "DONE")
-    (when (org-clock-is-active)
-      (org-clock-out)))
-   ))
-
-;; 状態変化のたびに上の関数を呼び出す
-(add-hook 'org-after-todo-state-change-hook 'org-clock-on-state-change)
-
-;; カレンダーから日付を選択し M/D(曜日) 形式の見出し文字列を返す
-;; 理由: org-captureテンプレートの日付見出しをカスタム形式で挿入するため
-(defun my/org-capture-date-heading ()
-  (let* ((time (org-read-date nil t nil "日付を選択: "))
-         (decoded (decode-time time))
-         (month (nth 4 decoded))
-         (day   (nth 3 decoded))
-         (dow   (format-time-string "%A" time)))
-    (format "%d/%d(%s)" month day dow)))
-
-;; org-capture 呼び出し前のバッファを記憶する変数
-;; 理由: キャプチャ先を「呼び出し時点のバッファ」にするために事前保存が必要
-(defvar my/org-capture-source-buffer nil)
-
-(advice-add 'org-capture :before
-            (lambda (&rest _)
-              (setq my/org-capture-source-buffer (current-buffer))))
-
-;; キャプチャ先を呼び出し時点のバッファの末尾に移動する
-(defun my/org-capture-to-source-buffer ()
-  (switch-to-buffer my/org-capture-source-buffer)
-  (goto-char (point-max)))
-
-;; C-c c でorg-captureを呼び出す
-(global-set-key (kbd "C-c c") 'org-capture)
-
-;; org-captureテンプレートの設定
-;; "d" を選択すると日次ログのテンプレートを現在のバッファ末尾に挿入する
-(setq org-capture-templates
-      '(("d" "日次ログ" entry
-         (function my/org-capture-to-source-buffer)
-         "* %(my/org-capture-date-heading)\n\n** TASK\n\n** TIL\n\n** 思いつき\n"
-         :empty-lines 1)))
-
-;; ob-swift / ob-kotlin / ob-typescript
-;; 理由: straight-use-package-by-default t の環境では :ensure t は不要かつ
-;;       package.el と straight.el が競合するため除去する
-(use-package ob-swift)
-(use-package ob-kotlin)
-(use-package ob-typescript)
-
-;; Org-babelで使う言語を有効化する
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((emacs-lisp . t)
-   (shell . t)
-   (swift . t)
-   (kotlin . t)
-   (typescript . t)))
-
-;;; ============================================================
-;;; シンタックスハイライト
-;;; ============================================================
-
-;; tree-sitter はコードをASTとして解析するため
-;; 正規表現ベースのハイライトより高精度・高速
-;; Emacs 29以降は組み込み（treesit）で利用可能
-(use-package treesit-auto
-  :config
-  ;; 必要な tree-sitter グラマーを自動インストールする設定
-  (setq treesit-auto-install 'prompt)
-  (global-treesit-auto-mode))
-
+  :bind
+  (("C-c a" . org-agenda)
+   ("C-c c" . org-capture)))
 ;;; ============================================================
 ;;; 補完システム
 ;;; ============================================================
@@ -695,7 +622,6 @@ DO NOT add an explanation or a body. Output ONLY the commit summary line."))
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/org/note.org"))
  '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
