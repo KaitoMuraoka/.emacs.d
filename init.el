5dd27dbc762fbfb64a281f877d31d481b098e720
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-04-03 09:46:03 +0900

    Setup engine-mode (#110)
    https://github.com/hrs/engine-mode
    
    # How to use 
     C-x /
    g : Search Google
    h : Search GitHub
    y : Search YouTube

--- init.el
+++ init.el
@@ -52,6 +52,24 @@
   (exec-path-from-shell-initialize))
 
 ;;; ============================================================
+;;; engine-mode
+;;; ============================================================
+(use-package engine-mode
+  :ensure t
+  :config
+  (setq browse-url-chrome-program "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");; Google Chrome に明示的に設定
+  (defengine google
+    "https://www.google.com/search?q=%s"
+    :keybinding "g")
+  (defengine github
+    "https://github.com/search?q=%s"
+    :keybinding "h")
+  (defengine youtube
+    "https://www.youtube.com/results?search_query=%s"
+    :keybinding "y")
+  (engine-mode t))
+
+;;; ============================================================
 ;;; 基本的な Emacs の設定
 ;;; ============================================================
 (set-language-environment "Japanese")
