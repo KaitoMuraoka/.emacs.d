31fcbf4a97c7e6caf1d5e70f2e1f3c7851bf276d
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-05-01 18:05:51 +0900

    feat(evil): DiredをEmacsキーバインドで使用できるように設定を追加

--- modules/mk-evil.el
+++ modules/mk-evil.el
@@ -19,6 +19,9 @@
   (evil-set-initial-state 'markdown-mode 'emacs)
   (evil-set-initial-state 'gfm-mode      'emacs)
 
+  ;; Dired でも Emacs キーバインドを維持する
+  (evil-set-initial-state 'dired-mode 'emacs)
+
   ;; ターミナルバッファでも Evil を無効化
   (evil-set-initial-state 'vterm-mode 'emacs)
   (evil-set-initial-state 'eat-mode   'emacs)
@@ -35,13 +38,17 @@
   ;; evil-collection が Magit モードに対して独自の state を設定するのを防ぐため
   ;; Magit を除外してから初期化する
   (setq evil-collection-mode-list
-        (remove 'magit evil-collection-mode-list))
+        (remove 'dired (remove 'magit evil-collection-mode-list)))
   (evil-collection-init))
 
 ;; Magit: evil-collection の初期化後に hook で強制的に emacs state にする
 ;; evil-set-initial-state だけでは evil-collection に上書きされるため hook を使う
 (with-eval-after-load 'magit
   (add-hook 'magit-mode-hook #'evil-emacs-state))
+
+;; Dired: evil-collection の初期化後に hook で強制的に emacs state にする
+(with-eval-after-load 'dired
+  (add-hook 'dired-mode-hook #'evil-emacs-state))
 
 ;; emacsclient で開いたコミットメッセージバッファを Emacs state にする
 ;; git-commit パッケージは magit の一部で standalone ロード不可のため
