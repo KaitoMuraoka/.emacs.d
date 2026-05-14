9d620ea2779810a90bc00cfa04ffa8934d5b133f
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-05-14 11:22:15 +0900

    setup claude-code and Monet

--- modules/mk-claude-code.el
+++ modules/mk-claude-code.el
@@ -1,16 +1,20 @@
+(use-package websocket
+  :straight t)
+
+(use-package monet
+  :straight (:type git :host github :repo "stevemolitor/monet" :branch "main" :depth 1)
+  :config
+  (monet-mode 1))
+
 (use-package claude-code
   :straight (:type git :host github :repo "stevemolitor/claude-code.el" :branch "main" :depth 1
                    :files ("*.el" (:exclude "images/*")))
   :bind-keymap
-  ("C-c c" . claude-code-command-map) ;; or your preferred key
-  ;; Optionally define a repeat map so that "M" will cycle thru Claude auto-accept/plan/confirm modes after invoking claude-code-cycle-mode / C-c M.
+  ("C-c a" . claude-code-command-map)
   :bind
   (:repeat-map my-claude-code-map ("M" . claude-code-cycle-mode))
   :config
-  ;; optional IDE integration with Monet
   (add-hook 'claude-code-process-environment-functions #'monet-start-server-function)
-  (monet-mode 1)
-
   (claude-code-mode))
 
 (provide 'mk-claude-code)
