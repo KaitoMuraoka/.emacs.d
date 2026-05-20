647f467693f185b3417057cc77ec0cc42b267d96
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-05-20 10:58:46 +0900

    fix: ショートカットキーを修正

--- modules/mk-org.el
+++ modules/mk-org.el
@@ -3,13 +3,13 @@
 ;;; ============================================================
 (defconst org-icloud-path
   "/Users/kaito.muraoka/Library/Mobile Documents/com~apple~CloudDocs/org/")
-(global-set-key (kbd "C-c o") (lambda () (interactive) (dired org-directory)))
+(global-set-key (kbd "C-c o") (lambda () (interactive) (dired org-icloud-path)))
 (use-package org
   :straight (:type built-in)
 
   :custom
   (org-directory org-icloud-path)
-  (org-default-notes-file (concat org-icloud-path "notes.org")))
+  (org-default-notes-file (concat org-icloud-path "notes.org"))
   (org-todo-keywords '((sequence "TODO" "DOING" "|" "DONE")))
   (org-clock-into-drawer t)
   (org-clock-persist t)
