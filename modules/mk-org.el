63725f4c00b518bec7d5751a730514e3beb8d795
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-07-13 19:38:33 +0900

    org-caputer の Tasks を追加

--- modules/mk-org.el
+++ modules/mk-org.el
@@ -11,6 +11,12 @@
   :custom
   (org-directory org-icloud-path)
   (org-default-notes-file (concat org-icloud-path "note.org"))
+  ;; capture テンプレート（%a を付けない）
+  (org-capture-templates
+   '(("t" "タスク" entry
+      (file org-default-notes-file)
+      "* TODO %?\n %u\n"
+      :empty-lines 1)))
 
   :bind
   ("C-c a" . org-agenda))
