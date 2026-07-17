bd4152f214416dfecb4dff53697f8909f0070873
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-07-17 14:38:56 +0900

    setup ai-code-interface.el (#174)
    https://github.com/tninja/ai-code-interface.el

--- init.el
+++ init.el
@@ -68,6 +68,7 @@
 (require 'mk-claude-code-ide)
 (require 'mk-claude-code)
 (require 'mk-agent-shell)
+(require 'mk-ai-code-interface)
 (require 'mk-dirvish)
 (require 'mk-org)
 (require 'mk-markdown)
@@ -83,6 +84,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ai-code-onboarding-seen t)
  '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
