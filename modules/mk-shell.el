32ee8dc7fc90c1e3744546bac6b92045298076d2
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-08-25 23:45:09 +0900

    eshell の行番号を非表示にする

--- modules/mk-shell.el
+++ modules/mk-shell.el
@@ -7,4 +7,9 @@
           (lambda ()
             (display-line-numbers-mode -1)))
 
+;; eshell バッファでは行番号を表示しない
+(add-hook 'eshell-mode-hook
+          (lambda ()
+            (display-line-numbers-mode -1)))
+
 (provide 'mk-shell)
