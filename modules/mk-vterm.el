91325bbe817775b2b23df94c79f7ac27d4fc7937
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-08-25 23:45:19 +0900

    vterm の行番号を非表示にする

--- modules/mk-vterm.el
+++ modules/mk-vterm.el
@@ -16,8 +16,9 @@
   ;;       Homebrew 等の PATH を引き継ぐため
   (vterm-shell (concat shell-file-name " -l"))
   :hook
-  ;; vterm バッファでは行番号・hl-line を無効化
+  ;; vterm バッファでは行番号を無効化
   (vterm-mode . (lambda ()
+                  (display-line-numbers-mode -1)
                   (with-editor-export-editor)
                   ;; $EDITOR ではなく、$GIT_EDITORを優先
                   (with-editor-export-git-editor)))
