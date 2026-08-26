2369ce100f8727910ad496722babd1d8c4389609
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-08-26 15:53:29 +0900

    ~/org  を参照するよう修正

--- modules/mk-org.el
+++ modules/mk-org.el
@@ -1,18 +1,21 @@
 ;;; ============================================================
 ;;; org-mode
 ;;; ============================================================
-(defconst org-icloud-path
-  "~/Library/Mobile Documents/com~apple~CloudDocs/org/")
-(global-set-key (kbd "C-c o") (lambda () (interactive) (dired org-icloud-path)))
+(defconst org-path
+  "~/org/")
+(global-set-key (kbd "C-c o") (lambda () (interactive) (dired org-path)))
 
 (use-package org
   :straight (:type built-in)
 
   :custom
-  (org-directory org-icloud-path)
-  (org-default-notes-file (concat org-icloud-path "note.org"))
-  ;; agenda はタスクを書き込む note.org のみを対象にする
-  (org-agenda-files (list (concat org-icloud-path "note.org")))
+  (org-directory org-path)
+  (org-default-notes-file (concat org-path "inbox.org"))
+  ;; agenda は til.org / routine.org / inbox.org / projects 配下の全ファイルを対象にする
+  (org-agenda-files (append (list (concat org-path "til.org")
+                                   (concat org-path "routine.org")
+                                   (concat org-path "inbox.org"))
+                             (directory-files-recursively (concat org-path "projects") "\\.org$")))
   ;; capture テンプレート（%a を付けない）
   (org-capture-templates
    '(("t" "タスク" entry
@@ -20,7 +23,7 @@
       "* TODO %?\n %u\n"
       :empty-lines 1)
      ("k" "ナレッジ" entry
-      (file "knowledge.org")
+      (file (concat org-path "til.org"))
       "* %?\n  # Wrote on %U"
       :empty-lines 1)))
 
